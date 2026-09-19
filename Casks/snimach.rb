04fb844254122f7e3d8e5864565de5002649b768
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-09-19 08:47:29 +0200

    Add the snimach cask

---
+++ Casks/snimach.rb
@@ -0,0 +1,30 @@
+cask "snimach" do
+  version "0.1.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/infomiho/snimach/releases/download/v#{version}/Snimach-#{version}-macOS-arm64.dmg"
+  name "Snimach"
+  desc "Menubar screenshots: capture with a hotkey, annotate, and paste"
+  homepage "https://snimach.miho.dev/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  app "Snimach.app"
+
+  uninstall quit:       "dev.twoducks.snimach",
+            login_item: "Snimach"
+
+  zap trash: [
+    "~/Library/Application Support/Snimach",
+    "~/Library/Caches/dev.twoducks.snimach",
+    "~/Library/HTTPStorages/dev.twoducks.snimach",
+    "~/Library/Preferences/dev.twoducks.snimach.plist",
+  ]
+end
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Homebrew casks for [infomiho](https://github.com/infomiho) apps.
 
 ```sh
 brew install --cask infomiho/tap/diktafon
+brew install --cask infomiho/tap/snimach
 ```
 
 The release workflow of each app updates its cask when a version is tagged.
