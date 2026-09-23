08b6a7a7756a5c60ec2b6bbc72f6659a00f2994a
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-09-23 17:16:42 +0200

    snimach 0.1.1

--- Casks/snimach.rb
+++ Casks/snimach.rb
@@ -1,6 +1,6 @@
 cask "snimach" do
-  version "0.1.0"
-  sha256 "d764b38eb1116185686331cbe77ab92cb86fe6bb045e76dfe2cf8527e339b396"
+  version "0.1.1"
+  sha256 "5590d3cc66229d670bab40bd004640b51ccb27bae04160505e3e3890060972f8"
 
   url "https://github.com/infomiho/snimach/releases/download/v#{version}/Snimach-#{version}-macOS-arm64.dmg"
   name "Snimach"
